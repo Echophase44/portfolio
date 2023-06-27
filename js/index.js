2f896731d967c75a2f9c0ba89e73479eab6d957e
Nick <Echophase44@github.com>
2023-06-27 14:14:22 -0400

    Created the details page for the cocktails app.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav_link");
+const logo = document.querySelector(".logo_img");
+const subtitle = document.querySelector(".section_subtitle-intro");
+const subtitleEgg = document.querySelector(".subtitle_egg");
 
+console.log(
+  `%c
+  __          __  _                          _ 
+  \\ \\        / / | |                        | |
+   \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___| |
+    \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ' _ \\ / _ \\ |
+     \\  /\\  /  __/ | (_| (_) | | | | | |  __/_|
+      \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)
+                                               
+                                               
+ `,
+  "font-family:monospace"
+);
+
+// Nav
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
 });
@@ -9,4 +27,10 @@ navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     document.body.classList.remove("nav-open");
   });
+});
+
+// Egg
+logo.addEventListener("click", () => {
+  logo.src = "img/logos/consolelogo.png";
+  console.log("You found the Logo Egg!");
 });
